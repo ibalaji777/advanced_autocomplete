28dac7b5ce75fff70a563ed4fed980d01f7b47c4
isattechnology <42474956+ibalaji777@users.noreply.github.com>
2020-03-24 03:56:15 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,275 @@
-/*
-	Prism by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
+// import {isat_ac} from './index'
 
-(function($) {
 
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
-	});
 
-	$(function() {
+// ]
+var data=[
+	{id:'1', name:'balaji',edu:'engineer' },
+	{id:'2', name:'sekar',edu:'eng'},
+	{id:'3', name:'yokesh',edu:'engineer'},
+	{id:'4', name:'durai',edu:'eng'},
+	{id:'5', name:'gabrie',edu:'eng'},
+	{id:'6', name:'Raghu',edu:'engineer'},
+	{id:'7', name:'Aravind',edu:'eng'},
+	{id:'8', name:'Deva',edu:'engineer'},
+	]
 
-		var	$window = $(window),
-			$body = $('body');
+// void (function() {
+isat_ac(document.getElementById("ac"),data,
+function(x,val){
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+    return   x.name.substr(0, val.length).toUpperCase() == val.toUpperCase() 
+},
+function(action,cursor,filtered,original_array,is_input_active){
+console.log("-----"+action+"->"+cursor+"-----")
+console.log(filtered)
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+var table=document.getElementById("just_1")
+var tbody=document.getElementById("just_2")
+var result=document.getElementById("result")
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+var trs=tbody.getElementsByTagName("tr");
 
-	});
 
-})(jQuery);
+if(action=="click"){
+
+    trs[cursor].style.background="red";
+
+    document.getElementById("ac").value=filtered[cursor].name;
+    table.style.display="none";
+    
+}
+
+
+if(is_input_active){
+
+    table.style.display="";
+
+//ui
+
+
+tbody.innerHTML="";
+var tr_data=[];
+
+ tr_data=original_array;
+
+for(var i=0;i<tr_data.length;i++)
+{
+
+var tr = document.createElement("tr");
+var td_data=tr_data[i];
+var text;
+for(var td_key in td_data){
+var td = document.createElement("td");
+
+text=document.createTextNode(td_data[td_key]);
+
+
+
+td.appendChild(text)
+tr.appendChild(td)
+
+}
+
+
+
+tbody.appendChild(tr);
+//ui
+
+
+
+}
+
+    console.log("cursor"+trs.length)
+
+
+      if(action=="enter"){
+
+        document.getElementById("ac").value=filtered[cursor].name;
+        table.style.display="none";
+    }
+     
+//mouse
+
+if(cursor!=-1){
+
+    trs[cursor].style.background="red";
+    // document.getElementById("ac").value=filtered[cursor].name;
+
+}
+
+
+}else{
+    cursor=-1;
+    // table.style.display="none";
+
+}
+
+},{
+
+    root:"just_1",   //class or id
+    selector:"TR", //.class or #id tagName(DIV,LIST)
+
+
+},
+['name','id']
+);
+
+//list
+isat_ac(document.getElementById("ul_input"),data,
+function(x,val){
+
+    return   x.name.substr(0, val.length).toUpperCase() == val.toUpperCase() 
+},
+function(action,cursor,filtered,original_array,is_input_active){
+console.log("-----"+action+"->"+cursor+"-----")
+console.log(filtered)
+
+var ul=document.getElementById("Just_2_ul")
+
+
+var lis=ul.getElementsByTagName("li");
+
+
+if(action=="click"){
+
+    lis[cursor].style.background="red";
+
+    document.getElementById("ul_input").value=filtered[cursor].name;
+    ul.style.display="none";
+    
+}
+
+
+if(is_input_active){
+
+    ul.style.display="";
+
+//ui
+
+
+ul.innerHTML="";
+var ul_data=[];
+
+ ul_data=original_array;
+
+for(var i=0;i<ul_data.length;i++)
+{
+
+var li = document.createElement("li");
+var li_data=ul_data[i];
+var text;
+
+text=document.createTextNode(li_data["name"]);
+
+console.log("li"+li_data["name"])
+
+li.appendChild(text)
+ul.appendChild(li);
+
+
+}
+
+ 
+
+      if(action=="enter"){
+
+        document.getElementById("ul_input").value=filtered[cursor].name;
+        ul.style.display="none";
+    }
+     
+//mouse
+
+if(cursor!=-1){
+
+    lis[cursor].style.background="red";
+    // document.getElementById("ac").value=filtered[cursor].name;
+
+}
+
+
+}else{
+    cursor=-1;
+    // table.style.display="none";
+
+}
+
+},{
+
+    root:"Just_2_ul",   //class or id
+    selector:"LI", //.class or #id tagName(DIV,LIST)
+
+
+},
+['name','id']
+);
+
+
+
+isat_ac(document.getElementById("ex-1"),data,
+function(x,val){
+
+	return;
+},
+function(action,cursor,filtered,original_array,is_input_active){
+
+document.getElementById("ex-1_data").innerHTML=JSON.stringify(filtered);
+document.getElementById("ex-1_input").innerHTML="input: "+is_input_active;
+document.getElementById("ex-1_action").innerHTML="action: "+action;
+document.getElementById("ex-1_cursor").innerHTML="cursor: "+cursor;
+
+
+},{
+root:"ex-1_table",   //class or id
+selector:"TR", //.class or #id tagName(DIV,LIST)
+
+},
+[]
+);
+
+
+
+
+isat_ac(document.getElementById("ex-2"),data,
+function(x,val){
+
+	return x.name.substr(0, val.length).toUpperCase() == val.toUpperCase()
+},
+function(action,cursor,filtered,original_array,is_input_active){
+
+document.getElementById("ex-2_data").innerHTML=JSON.stringify(filtered);
+document.getElementById("ex-2_input").innerHTML="input: "+is_input_active;
+document.getElementById("ex-2_action").innerHTML="action: "+action;
+document.getElementById("ex-2_cursor").innerHTML="cursor: "+cursor;
+
+
+},{
+root:"ex-2_table",   //class or id
+selector:"TR", //.class or #id tagName(DIV,LIST)
+
+},
+['name','edu','id']
+);
+
+
+    isat_ac(document.getElementById("ex-3"),data,
+    function(x,val){
+    
+        return x.name.substr(0, val.length).toUpperCase() == val.toUpperCase() || x.edu.substr(0, val.length).toUpperCase() == val.toUpperCase()
+    },
+    function(action,cursor,filtered,original_array,is_input_active){
+    
+    document.getElementById("ex-3_data").innerHTML=JSON.stringify(filtered);
+    document.getElementById("ex-3_input").innerHTML="input: "+is_input_active;
+    document.getElementById("ex-3_action").innerHTML="action: "+action;
+    document.getElementById("ex-3_cursor").innerHTML="cursor: "+cursor;
+    
+    
+    },{
+    root:"ex-3_table",   //class or id
+    selector:"TR", //.class or #id tagName(DIV,LIST)
+    
+    },
+    ['name','edu','id']
+    );
